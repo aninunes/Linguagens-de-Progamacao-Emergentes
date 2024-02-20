ed3152b7dd01302dfedf59e090e5e35ce643c916
Anita Maria Santos Cortes Nunes <20201PF.CC0041@passofundo.ifsul.local>
2024-02-20 09:03:14 -0300

    Aula 1

---
+++ OperadorTernario.js
@@ -0,0 +1,10 @@
+let resultado = "";
+
+let preco = 50;
+
+if(preco < 40){
+    resultado = "Barato";
+}else{
+    resultado = "Caro";
+}
+console.log(resultado);
---
+++ Variaveis.js
@@ -0,0 +1,7 @@
+let texto ="LPE";
+console.log(texto);
+
+texto = "Linguagens de Programação Emergentes"
+console.log(texto);
+const ano = 2024;
+console.log(ano);
